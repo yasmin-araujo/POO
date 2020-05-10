0ec9debe23c94fd74087201e5c5e0eed737b7ea8
yasmin_araujo <yasminoa@hotmail.com>
2020-05-09 22:43:16 -0300

    Change inheritance of exercise 3

--- Lista3/Lista03/src/Quadrado.java
+++ Lista3/Lista03/src/Quadrado.java
@@ -1,20 +1,7 @@
-public class Quadrado extends FigurasGeometricas
+public class Quadrado extends Retangulo
 {
-    private double lado;
-
     Quadrado(double lado, String cor, boolean filled)
     {
-        super(cor, filled);
-        this.lado = lado;
-    }
-
-    public double area(double lado)
-    {
-        return super.area(lado, lado);
-    }
-
-    public double perimetro(double lado)
-    {
-        return super.perimetro(lado, lado);
+        super(lado, lado, cor, filled);
     }
 }
