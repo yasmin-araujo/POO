04865e7965fd23a9e75678012d24a9972d5d9aa4
yasmin_araujo <yasminoa@hotmail.com>
2020-05-09 22:43:58 -0300

    Check if person already exists

--- Lista3/Lista03/src/Agenda.java
+++ Lista3/Lista03/src/Agenda.java
@@ -5,16 +5,24 @@ public class Agenda
 {
     private List<Pessoa> pessoas = new ArrayList<>();
 
-    public void addContato(String CPF, String nome, String endereco, String data_nasc, String email, String estado_civil)
+    public void addContato(String CPF, String nome, String endereco, String data_nasc, String email,
+                           String estado_civil)
     {
-        Pessoa p = new PessoaFisica(CPF, nome, endereco, data_nasc, email, estado_civil);
-        pessoas.add(p);
+        if (pesquisaNome(nome) == -1 && pesquisaDoc(CPF) == -1)
+        {
+            Pessoa p = new PessoaFisica(CPF, nome, endereco, data_nasc, email, estado_civil);
+            pessoas.add(p);
+        }
     }
 
-    public void addContato(String CNPJ, String nome, String endereco, String email, int inscricao_estadual, String razao_social)
+    public void addContato(String CNPJ, String nome, String endereco, String email, int inscricao_estadual,
+                           String razao_social)
     {
-        Pessoa p = new PessoaJuridica(CNPJ, nome, endereco, email, inscricao_estadual, razao_social);
-        pessoas.add(p);
+        if (pesquisaNome(nome) == -1 && pesquisaDoc(CNPJ) == -1)
+        {
+            Pessoa p = new PessoaJuridica(CNPJ, nome, endereco, email, inscricao_estadual, razao_social);
+            pessoas.add(p);
+        }
     }
 
     public void removeContato(int index)
@@ -26,7 +34,7 @@ public class Agenda
     {
         for (int i = 0; i < pessoas.size(); i++)
         {
-            if(pessoas.get(i).getNome() == nome) return i;
+            if (pessoas.get(i).getNome() == nome) return i;
         }
         return -1;
     }
@@ -35,16 +43,16 @@ public class Agenda
     {
         for (int i = 0; i < pessoas.size(); i++)
         {
-            if(pessoas.get(i) instanceof PessoaFisica)
+            if (pessoas.get(i) instanceof PessoaFisica)
             {
                 PessoaFisica pf = (PessoaFisica) pessoas.get(i);
-                if(pf.getCPF() == doc)
+                if (pf.getCPF() == doc)
                     return i;
             }
             else
             {
                 PessoaJuridica pj = (PessoaJuridica) pessoas.get(i);
-                if(pj.getCNPJ() == doc)
+                if (pj.getCNPJ() == doc)
                     return i;
             }
         }
@@ -54,21 +62,22 @@ public class Agenda
     public void ordenaContatos()
     {
         // Coloca as pessoas físicas no começo
-        pessoas.sort( (p1, p2) -> {
-            if(p1 instanceof PessoaFisica && p2 instanceof PessoaJuridica)
+        pessoas.sort((p1, p2) -> {
+            if (p1 instanceof PessoaFisica && p2 instanceof PessoaJuridica)
                 return -1;
             else return 1;
-        } );
+        });
 
         // Ordena pelo CPF/CNPJ
-        pessoas.sort( (p1, p2) -> {
+        pessoas.sort((p1, p2) -> {
             // Caso sejam pessoas físicas ordena pelo CPF
-            if(p1 instanceof PessoaFisica && p2 instanceof PessoaFisica)
+            if (p1 instanceof PessoaFisica && p2 instanceof PessoaFisica)
                 return Long.valueOf(((PessoaFisica) p1).getCPF()) > Long.valueOf(((PessoaFisica) p2).getCPF()) ? 1 : -1;
 
             // Caso sejam jurídicas ordena pelo CNPJ
-            if(p1 instanceof PessoaJuridica && p2 instanceof PessoaJuridica)
-                return Long.valueOf(((PessoaJuridica) p1).getCNPJ()) > Long.valueOf(((PessoaJuridica) p2).getCNPJ()) ? 1 : -1;
+            if (p1 instanceof PessoaJuridica && p2 instanceof PessoaJuridica)
+                return Long.valueOf(((PessoaJuridica) p1).getCNPJ()) > Long.valueOf(((PessoaJuridica) p2).getCNPJ())
+                        ? 1 : -1;
 
             // Senão mantem ordem
             return 0;
@@ -79,15 +88,17 @@ public class Agenda
     {
         for (int i = 0; i < pessoas.size(); i++)
         {
-            if(pessoas.get(i) instanceof PessoaFisica)
+            if (pessoas.get(i) instanceof PessoaFisica)
             {
                 PessoaFisica pf = (PessoaFisica) pessoas.get(i);
-                System.out.println("\nContato " + i + "\nCPF:" + pf.getCPF() + "\nNome: " + pf.getNome() + "\nEndereço: " + pf.getEndereco() + "\nData de nascimento: " + pf.getData_nasc() + "\nEmail: " + pf.getEmail() + "\nEstado civil: " + pf.getEstado_civil());
+                System.out.println("\nContato " + i + "\nCPF:" + pf.getCPF() + "\nNome: " + pf.getNome() + "\nEndere" +
+                        "ço: " + pf.getEndereco() + "\nData de nascimento: " + pf.getData_nasc() + "\nEmail: " + pf.getEmail() + "\nEstado civil: " + pf.getEstado_civil());
             }
             else
             {
                 PessoaJuridica pj = (PessoaJuridica) pessoas.get(i);
-                System.out.println("\nContato " + i + "\nCNPJ:" + pj.getCNPJ() + "\nNome: " + pj.getNome() + "\nEndereço: " + pj.getEndereco() + "\nEmail: " + pj.getEmail() + "\nInscrição estadual: " + pj.getInscricao_estadual() + "\nRazão social: " + pj.getRazao_social());
+                System.out.println("\nContato " + i + "\nCNPJ:" + pj.getCNPJ() + "\nNome: " + pj.getNome() +
+                        "\nEndereço: " + pj.getEndereco() + "\nEmail: " + pj.getEmail() + "\nInscrição estadual: " + pj.getInscricao_estadual() + "\nRazão social: " + pj.getRazao_social());
             }
         }
     }
